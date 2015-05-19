6c2058e794c9cbfc33fca43bb2018e7f92db9074
Adriano Martins <adrianomartins@gmail.com>
2015-05-19 01:14:10 -0300

    Revert "Prevent leaking surfaces from exiting windows"
    This reverts commit 0fc0b8bcf48f5d0627f04c3c015a895caa3ea073.

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -4694,7 +4694,6 @@ public class WindowManagerService extends IWindowManager.Stub
             }
 
             final long origId = Binder.clearCallingIdentity();
-            wtoken.inPendingTransaction = false;
             setTokenVisibilityLocked(wtoken, null, visible, AppTransition.TRANSIT_UNSET,
                     true, wtoken.voiceInteraction);
             wtoken.updateReportedVisibilityLocked();
